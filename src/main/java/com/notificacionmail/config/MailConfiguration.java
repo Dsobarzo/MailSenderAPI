ba5c7ffdde9c19ec42c8c2f28f3b59ca8abd0500
dsobarzo <diego.sobarzo.o@gmail.com>
2023-11-24 21:04:38 -0300

    feat (COnfiguracion de mail Sender)

--- src/main/java/com/notificacionmail/config/MailConfiguration.java
+++ src/main/java/com/notificacionmail/config/MailConfiguration.java
@@ -4,12 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailConfiguration {
 
     public JavaMailSender getJavaMailSender(){
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("sistemas.salud@umag.cl");
+        mailSender.setPassword("Hand.86.Xv");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol","smtp");
+        props.put("mail.smtp.auth","true");
+        props.put("mail.smtp.starttls", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 
 }
