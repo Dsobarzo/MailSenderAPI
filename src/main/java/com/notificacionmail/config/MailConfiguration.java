c1b81f1796f49073009265c0a6528e572568ed3e
dsobarzo <diego.sobarzo.o@gmail.com>
2023-11-24 21:06:57 -0300

    feat (Configuracion de mail Sender: debug true)

--- src/main/java/com/notificacionmail/config/MailConfiguration.java
+++ src/main/java/com/notificacionmail/config/MailConfiguration.java
@@ -21,8 +21,8 @@ public class MailConfiguration {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol","smtp");
         props.put("mail.smtp.auth","true");
-        props.put("mail.smtp.starttls", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.smtp.starttls","true");
+        props.put("mail.debug","true");
 
         return mailSender;
     }
