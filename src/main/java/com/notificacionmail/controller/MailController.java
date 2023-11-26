d3994d053cdeec6fc4aecc384f81a9abc2eeb259
dsobarzo <diego.sobarzo.o@gmail.com>
2023-11-26 20:38:01 -0300

    feat (Controller: ResquestMapping)

--- src/main/java/com/notificacionmail/controller/MailController.java
+++ src/main/java/com/notificacionmail/controller/MailController.java
@@ -5,10 +5,7 @@ import com.notificacionmail.domain.EmailFileDTO;
 import com.notificacionmail.service.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -19,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/v1")
 public class MailController {
 
     @Autowired
