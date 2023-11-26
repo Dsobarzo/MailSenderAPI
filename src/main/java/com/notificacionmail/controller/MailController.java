2c3410dc9fbd40e33bf7f131ae9ee67616c6a2dc
dsobarzo <diego.sobarzo.o@gmail.com>
2023-11-26 20:33:14 -0300

    feat (Domain, Controller: EndPoint Config)

---
+++ src/main/java/com/notificacionmail/controller/MailController.java
@@ -0,0 +1,65 @@
+package com.notificacionmail.controller;
+
+import com.notificacionmail.domain.EmailDTO;
+import com.notificacionmail.domain.EmailFileDTO;
+import com.notificacionmail.service.IEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MailController {
+
+    @Autowired
+    private IEmailService emailService;
+    @PostMapping("/sendMail")
+    public ResponseEntity<?> receiveRequestEmail(@RequestBody EmailDTO emailDTO){
+
+        System.out.println("Mensaje Recibido " + emailDTO);
+
+        emailService.sendEmail(emailDTO.getToUser(), emailDTO.getSubject(), emailDTO.getMessage());
+
+        Map<String, String> response = new HashMap<>();
+        response.put("estado", "Enviado");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/sendMessageFile")
+    public ResponseEntity<?> receiveRequestEmailWithFile(@ModelAttribute EmailFileDTO emailFileDTO){
+
+        try {
+            String fileName = emailFileDTO.getFile().getOriginalFilename();
+
+            Path path = Paths.get("src/mail/resources/files/" + fileName);
+
+            Files.createDirectories(path.getParent());
+            Files.copy(emailFileDTO.getFile().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+            File file = path.toFile();
+
+            emailService.sendEmailWithFile(emailFileDTO.getToUser(), emailFileDTO.getSubject(), emailFileDTO.getMessage(), file);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("estado", "Enviado");
+            response.put("archivo", fileName);
+
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e){
+            throw new RuntimeException("Error al enviar el Email con el archivo. " + e.getMessage());
+        }
+    }
+
+}
