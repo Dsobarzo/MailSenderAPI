4185835c38e49381fd1d44c7cc0c5d0c14c397c7
dsobarzo <diego.sobarzo.o@gmail.com>
2023-12-08 16:36:01 -0300

    feat (Cors : Allowed * )

--- src/main/java/com/notificacionmail/NotificacionmailApplication.java
+++ src/main/java/com/notificacionmail/NotificacionmailApplication.java
@@ -19,7 +19,7 @@ public class NotificacionmailApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").
-
+						allowedOrigins("*").
 						allowedMethods("*").
 						allowedHeaders("*");
 			}
